a5229987a484a2c006d334998d8b0ddb8a6fdad9
Matt Terry <matt.terry@gmail.com>
2018-11-26 13:58:56 -0800

    Publish to pypi when we cut a release (#35)
    * publish to pypi when we cut a release
    
    * build universal wheels
    
    * Add secure PyPI password
    
    * use xenail. fix lint error
    
    * unbreak boto
    
    * add py3.6 and py3.7 classifiers

--- setup.py
+++ setup.py
@@ -51,6 +51,8 @@ classes = """
     Programming Language :: Python
     Programming Language :: Python :: 2.7
     Programming Language :: Python :: 3.5
+    Programming Language :: Python :: 3.6
+    Programming Language :: Python :: 3.7
     Programming Language :: Python :: Implementation :: CPython
     Operating System :: OS Independent
 """
