ecd17b4e4623aa652f825af71957feb86436b158
Matthew Dickinson <mdickinson@nextdoor.com>
2020-04-15 17:22:49 -0600

    Pin jinja2.

--- setup.py
+++ setup.py
@@ -77,6 +77,7 @@ setup(
         'nose==1.3.7',
         'moto==1.3.15.dev122',
         'MarkupSafe==1.1.1',
+        'Jinja2==2.10.3',
     ],
     test_suite='nose.collector',
     install_requires=[
